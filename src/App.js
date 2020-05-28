6c61dbba822cb50f1659851512e9ec5721b42302
Bharath <bharath98mohan@gmail.com>
2020-05-28 18:49:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FaReact } from 'react-icons/fa';
+import { FiBriefcase } from 'react-icons/fi';
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FaReact color= 'blue' size= '5rem'/>
+      <FiBriefcase color= 'blue' size= '5rem'/>
     </div>
+    
   );
 }
 
